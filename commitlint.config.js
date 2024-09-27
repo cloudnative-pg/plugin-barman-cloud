3ffd74a8c9b6a1d5385b70803f3dfe607d373925
Francesco Canovai <francesco.canovai@enterprisedb.com>
2024-09-27 09:56:01 +0200

    ci: warn on long commit body line
    Avoid failing the commitlint task on long commit body lines and just
    raise a warning. This allows renovate tabled commit messages to pass the
    ci.
    
    Signed-off-by: Francesco Canovai <francesco.canovai@enterprisedb.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,7 @@ const Configuration= {
     rules: {
         'body-empty': [1, 'never'],
         'body-case': [2, 'always', 'sentence-case'],
+        'body-max-line-length': [1, 'always', 100],
         'references-empty': [1, 'never'],
         'signed-off-by': [2, 'always', 'Signed-off-by:'],
     },
