8fda1c87558821d46855f6cc617a836ab48d5326
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2025-03-05 18:21:00 +0100

    feat: retention policy
    This commit makes the Barman cloud plugin support the enforcement of
    retention policy as provided by the barman-cloud tool suite.
    
    The first recoverability point and the last successful backup are
    shown in the status of the ObjectStore resource for each involved
    server name.
    
    feat: implement recovery window
    
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- api/v1/objectstore_types.go
+++ api/v1/objectstore_types.go
@@ -27,11 +27,26 @@ type InstanceSidecarConfiguration struct {
 	// The environment to be explicitly passed to the sidecar
 	// +optional
 	Env []corev1.EnvVar `json:"env,omitempty"`
+
+	// The retentionCheckInterval defines the frequency at which the
+	// system checks and enforces retention policies.
+	// +kubebuilder:default:=1800
+	// +optional
+	RetentionPolicyIntervalSeconds int `json:"retentionPolicyIntervalSeconds,omitempty"`
 }
 
 // ObjectStoreSpec defines the desired state of ObjectStore.
 type ObjectStoreSpec struct {
+	// The configuration for the barman-cloud tool suite
 	Configuration barmanapi.BarmanObjectStoreConfiguration `json:"configuration"`
+
+	// RetentionPolicy is the retention policy to be used for backups
+	// and WALs (i.e. '60d'). The retention policy is expressed in the form
+	// of `XXu` where `XX` is a positive integer and `u` is in `[dwm]` -
+	// days, weeks, months.
+	// +kubebuilder:validation:Pattern=^[1-9][0-9]*[dwm]$
+	// +optional
+	RetentionPolicy string `json:"retentionPolicy,omitempty"`
 
 	// +optional
 	InstanceSidecarConfiguration InstanceSidecarConfiguration `json:"instanceSidecarConfiguration,omitempty"`
@@ -39,8 +54,21 @@ type ObjectStoreSpec struct {
 
 // ObjectStoreStatus defines the observed state of ObjectStore.
 type ObjectStoreStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// ServerRecoveryWindow maps each server to its recovery window
+	ServerRecoveryWindow map[string]RecoveryWindow `json:"serverRecoveryWindow,omitempty"`
+}
+
+// RecoveryWindow represents the time span between the first
+// recoverability point and the last successful backup of a PostgreSQL
+// server, defining the period during which data can be restored.
+type RecoveryWindow struct {
+	// The first recoverability point in a PostgreSQL server refers to
+	// the earliest point in time to which the database can be
+	// restored.
+	FirstRecoverabilityPoint *metav1.Time `json:"firstRecoverabilityPoint,omitempty"`
+
+	// The last successful backup time
+	LastSuccessfulBackupTime *metav1.Time `json:"lastSuccussfulBackupTime,omitempty"`
 }
 
 // +kubebuilder:object:root=true
